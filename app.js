6b142f5ab117fb34b305fa6fd10d44bccd51a34a
gimenados <gimenasande1@gmail.com>
2024-02-27 17:24:41 -0300

    json

---
+++ app.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const boton = document.getElementById("button");
+    
+    boton.addEventListener("click", function() {
+        console.log("Boton presionado");
+    })
+    
+    // function searchProducts() {
+    //     //Creo la variable del boton busqueda le tomo los valores y las letras en minuscula
+    //     var producName = document.getElementById('productName').value.toLocaleLowerCase();  
+        
+    // }
+
+})
